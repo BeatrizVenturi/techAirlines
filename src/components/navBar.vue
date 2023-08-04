9b9fadcfaa9d2ad3f319036ccd7f5aa404e5ca21
DanielaVenturi <danielaventuri14@gmail.com>
2023-08-04 16:49:28 -0300

    navbar

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,27 +1,60 @@
 <script setup>
 </script>
 <template>
-    <nav>
-        <div class="nav-bar">
-
-            <a href="">
-                <RouterLink to="/">Menu</RouterLink>
-            </a>
-            <a href="">
-                <RouterLink to="/about">Hotéis</RouterLink>
-            </a>
-        </div>
-
-    </nav>
+   
+   <nav class="nav">
+	<ul>
+		<li><a href="#"><RouterLink to="/">Menu</RouterLink></a></li>
+    <li><a href="#"><RouterLink to="/about">Hotéis</RouterLink>
+</a></li>
+		
+	</ul>
+</nav>
+   
+   
 </template>
 <style scoped>
-.nav-bar {
-    background-color: aqua;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    position: fixed;
-    top: 0;
-    width: 100%
-
+nav {
+	margin: 0 auto;
+	background: #3253A8;
+	
 }
+
+nav::after {
+	display: block;
+	content: '';
+	clear: both;
+}
+
+nav ul {
+	padding: 0;
+	margin: 0;
+	list-style: none;
+}
+
+nav ul li {
+	float: left;
+	position: relative;
+}
+
+nav ul li a {
+	display: block;
+	color: #ffff;
+	text-decoration: none;
+	padding: 1rem 1rem;
+	transition: all .3s ease-in-out;
+}
+
+nav ul  a:hover,
+nav ul  a:focus {
+	background: #1e3b839c;
+}
+
+
+nav ul a:focus {
+	background: #eee;
+	color: #111;
+}
+
+
 </style>
