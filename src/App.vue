1932fd3382ee90f7842855c3f0c890b39c960e21
BeatrizVenturi <venturibeatiz329@gmail.com>
2023-08-04 10:45:16 -0300

    inicio

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+      <nav>
+
+        <RouterLink to="/">Menu</RouterLink>
+        <RouterLink to="/about">Hotéis</RouterLink>
+      </nav>
+    
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
